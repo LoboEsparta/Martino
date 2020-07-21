adc1e87176d9fc50f1da38389a54ac4b3d325b1d
unknown <a3519110011@alumno.uttehuacan.edu.mx>
2020-07-21 15:51:06 -0500

    Eliminado el repositorio

--- Pagina M/JS/abrir.js
+++
@@ -1,4 +0,0 @@
-$('.Menu-Bot').on('click', function () {
-    $('#sidebar-container').toggleClass ('abrir');
-    
-});
--- Pagina M/php/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "pedidos";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
